b79b530e5ced3e9ad94309db8018106e1ae005a5
Your Name <you@example.com>
2022-03-03 19:54:01 +0530

    Day 2

---
+++ BMI-CALCLATOR.py
@@ -0,0 +1,7 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+a = float(weight)
+b = float(height)
+result = a/(b*b)
+print(int(result))
---
+++ variable and type casting.py
@@ -0,0 +1,8 @@
+# hashtag is used to comment lines in python.
+num = len(input("Drop a message "))
+Char = input("To test type")
+print(num)
+print(type(num))
+print(type(Char))
+new_num_char = str(num)
+print(type(new_num_char))
